abaeb1712875948ed8a3d0d2594ff375db2dbd93
Radovan Synek <rsynek@redhat.com>
2021-06-09 11:10:40 +0200

    Define the maximumCapacity constraint

---
+++ src/main/java/org/kie/baaas/solver/ServiceDeploymentConstraintProvider.java
@@ -0,0 +1,56 @@
+package org.kie.baaas.solver;
+
+import org.kie.baaas.domain.ResourceCapacity;
+import org.kie.baaas.domain.Service;
+import org.kie.baaas.domain.ResourceRequirement;
+import org.optaplanner.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.core.api.score.stream.Joiners;
+
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.sumLong;
+
+public class ServiceDeploymentConstraintProvider implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[] {
+                maximumCapacity(constraintFactory),
+                cost(constraintFactory)
+        };
+    }
+
+    Constraint maximumCapacity(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(Service.class)
+                .join(ResourceCapacity.class, Joiners.equal(Service::getOsdCluster, ResourceCapacity::getOsdCluster))
+                .join(ResourceRequirement.class,
+                        Joiners.equal((service, resourceCapacity) -> service, ResourceRequirement::getService),
+                        Joiners.equal((service, resourceCapacity) -> resourceCapacity.getResource(), ResourceRequirement::getResource)
+                )
+                .groupBy(
+                        (service, resourceCapacity, resourceRequirement) -> service.getOsdCluster(),
+                        (service, resourceCapacity, resourceRequirement) -> resourceCapacity,
+                        sumLong((service, resourceCapacity, resourceRequirement) -> resourceRequirement.getAmount()))
+                .filter((cluster, resourceCapacity, usagePerCluster) -> usagePerCluster > resourceCapacity.getCapacity())
+                .penalizeLong("resourceCapacity", HardSoftLongScore.ONE_HARD,
+                        (cluster, resourceCapacity, usagePerCluster) -> usagePerCluster - resourceCapacity.getCapacity());
+
+    }
+/*
+    return factory.from(MrMachineCapacity.class)
+            .join(MrProcessAssignment.class,
+                  equal(MrMachineCapacity::getMachine, MrProcessAssignment::getMachine))
+            .groupBy((machineCapacity, processAssignment) -> machineCapacity.getMachine(),
+            (machineCapacity, processAssignment) -> machineCapacity,
+    sumLong((machineCapacity, processAssignment) -> processAssignment
+            .getUsage(machineCapacity.getResource())))
+            .filter(((machine, machineCapacity, usage) -> machineCapacity.getMaximumCapacity() < usage))
+            .penalizeLong(MrConstraints.MAXIMUM_CAPACITY, HardSoftLongScore.ONE_HARD,
+                        (machine, machineCapacity, usage) -> usage - machineCapacity.getMaximumCapacity());
+   */
+
+    Constraint cost(ConstraintFactory constraintFactory) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/org/kie/baaas/solver/ServiceDeploymentConstraintProviderTest.java
@@ -0,0 +1,59 @@
+package org.kie.baaas.solver;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.kie.baaas.domain.OsdCluster;
+import org.kie.baaas.domain.Resource;
+import org.kie.baaas.domain.ResourceCapacity;
+import org.kie.baaas.domain.ResourceRequirement;
+import org.kie.baaas.domain.Service;
+import org.kie.baaas.domain.ServiceDeploymentSchedule;
+import org.optaplanner.test.api.score.stream.ConstraintVerifier;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ServiceDeploymentConstraintProviderTest {
+
+    @Inject
+    ConstraintVerifier<ServiceDeploymentConstraintProvider, ServiceDeploymentSchedule> constraintVerifier;
+
+    @Test
+    void maximumCapacity() {
+        Resource cpu = new Resource();
+        Resource memory = new Resource();
+
+        OsdCluster cluster1 = new OsdCluster(1L, 0.5);
+        OsdCluster cluster2 = new OsdCluster(2L, 0.5);
+
+        ResourceCapacity cpuCapacityCluster1 = new ResourceCapacity(cluster1, cpu, 10L);
+        ResourceCapacity memoryCapacityCluster1 = new ResourceCapacity(cluster1, cpu, 10L);
+        ResourceCapacity cpuCapacityCluster2 = new ResourceCapacity(cluster2, cpu, 10L);
+        ResourceCapacity memoryCapacityCluster2 = new ResourceCapacity(cluster2, cpu, 10L);
+
+        Service service1 = new Service("service-1");
+        Service service2 = new Service("service-2");
+        Service service3 = new Service("service-3");
+        service1.setOsdCluster(cluster1);
+        service2.setOsdCluster(cluster2);
+        service3.setOsdCluster(cluster2);
+
+        ResourceRequirement cpuRequirementService1 = new ResourceRequirement(service1, cpu, 3L);
+        ResourceRequirement memoryRequirementService1 = new ResourceRequirement(service1, memory, 3L);
+        ResourceRequirement cpuRequirementService2 = new ResourceRequirement(service2, cpu, 30L);
+        ResourceRequirement memoryRequirementService2 = new ResourceRequirement(service2, memory, 30L);
+        ResourceRequirement cpuRequirementService3 = new ResourceRequirement(service3, cpu, 300L);
+        ResourceRequirement memoryRequirementService3 = new ResourceRequirement(service3, memory, 300L);
+
+        /*
+           cluster1: (10 - 3) + (10 - 3)  = no penalty
+           cluster2: (10 - 300 - 30) + (10 - 300 - 30) = -640
+         */
+        constraintVerifier.verifyThat(ServiceDeploymentConstraintProvider::maximumCapacity)
+                .given(cpu, memory, cluster1, cluster2, cpuCapacityCluster1, memoryCapacityCluster1, cpuCapacityCluster2,
+                        memoryCapacityCluster2, service1, service2, service3, cpuRequirementService1, memoryRequirementService1,
+                        cpuRequirementService2, memoryRequirementService2, cpuRequirementService3, memoryRequirementService3)
+                .penalizesBy(640L);
+    }
+}
